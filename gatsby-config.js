240c312e10374b577630f1fbf6a31a978c2f9687
maxxoto <ahmatdanis24@gmail.com>
2019-08-12 09:45:45 +0700

    :roket: Fix perfomance and caching. Also fix gatsby-config.js build failed

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,43 +30,6 @@ module.exports = {
         purgeOnly: ["src/css/style.css", "src/css/global.css"]
       }
     },
-    `gatsby-plugin-offline`,
-    {
-      options: {
-        importWorkboxFrom: `local`,
-        globDirectory: rootDir,
-        globPatterns,
-        modifyUrlPrefix: {
-          // If `pathPrefix` is configured by user, we should replace
-          // the default prefix with `pathPrefix`.
-          "/": `${pathPrefix}/`,
-        },
-        cacheId: `gatsby-plugin-offline`,
-        // Don't cache-bust JS or CSS files, and anything in the static directory,
-        // since these files have unique URLs and their contents will never change
-        dontCacheBustUrlsMatching: /(\.js$|\.css$|static\/)/,
-        runtimeCaching: [
-          {
-            // Use cacheFirst since these don't need to be revalidated (same RegExp
-            // and same reason as above)
-            urlPattern: /(\.js$|\.css$|static\/)/,
-            handler: `cacheFirst`,
-          },
-          {
-            // Add runtime caching of various other page resources
-            urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
-            handler: `staleWhileRevalidate`,
-          },
-          {
-            // Google Fonts CSS (doesn't end in .css so we need to specify it)
-            urlPattern: /^https?:\/\/fonts\.googleapis\.com\/css/,
-            handler: `staleWhileRevalidate`,
-          },
-        ],
-        skipWaiting: true,
-        clientsClaim: true,
-      }
-
-    },
+    `gatsby-plugin-offline`
   ]
 };
